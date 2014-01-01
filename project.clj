828fc1fd6a969a4f1d956370e27edf5721cbeb59
Linus Ericsson <oscarlinusericsson@gmail.com>
2014-01-01 20:16:07 +0100

    added some libs to play with

---
+++ project.clj
@@ -0,0 +1,35 @@
+(defproject nebulosity "0.1.0-SNAPSHOT"
+  :description "Clojure code surrounded by nebulosity, eventually to reach star state"
+  :url "https://github.com/claj/nebulosity"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [enlive "1.1.5"]
+                 [bytebuffer "0.2.0"]
+                 [cc.artifice/clj-ml "0.5.0-SNAPSHOT"]
+                 [gloss "0.2.2"]
+                 [reduce-fsm "0.1.0"]
+                 [com.cemerick/pomegranate "0.2.0"]
+                 [org.clojure/core.logic "0.8.5"]
+                 [org.clojure/core.match "0.2.0"]
+                 [org.clojure/math.numeric-tower "0.0.3"]
+                 [org.clojure/core.contracts "0.0.5"]
+                 [org.clojure/data.priority-map "0.0.4"]
+                 [org.clojure/test.generative "0.5.0"]
+                 [org.clojure/data.xml "0.0.7"]
+                 [org.clojure/data.zip "0.1.1"]
+                 [org.clojure/data.generators "0.1.2"]
+                 [org.clojure/core.cache "0.6.3"]
+                 [org.clojure/core.unify "0.5.5"]
+                 [org.clojure/core.memoize "0.5.6"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.clojure/tools.trace "0.7.6"]
+                 [org.clojure/data.json "0.2.3"]
+                 [org.clojure/data.csv "0.1.2"]
+                 [org.clojure/java.jmx "0.2.0"]
+                 [org.clojure/java.data "0.1.1"]
+                 [org.clojure/tools.macro "0.1.2"]
+                 [org.clojure/data.fressian "0.2.0"]
+                 [org.clojure/data.finger-tree "0.0.1"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [criterium "0.4.2"]])
---
+++ test/nebulosity/core_test.clj
@@ -0,0 +1,7 @@
+(ns nebulosity.core-test
+  (:require [clojure.test :refer :all]
+            [nebulosity.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
