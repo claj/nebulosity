9712ca732b4aa3b270241841eed4be5941612d5c
Linus Ericsson <oscarlinusericsson@gmail.com>
2014-01-08 07:24:15 +0100

    got some highlights to work

--- src/nebulosity/scrolls.clj
+++ src/nebulosity/scrolls.clj
@@ -1,34 +1,54 @@
 (ns nebulosity.scrolls
-  (:require [seesaw.core :as s]
-            [seesaw.table :as t]
+  "nice highlights on tables would be nice"
+  (:use [clojure repl pprint]
+        [seesaw swingx core table]))
 
-)
-(:use [clojure.repl]             [seesaw.swingx])
-)
+(native!)
 
+(def fr (frame :title "highlights"))
+(.setAlwaysOnTop fr true)
 
-(def fr (s/frame))
-
+;;works!
 (defn hl-predicate [renderer adapter]
-  (> (.getValueAt adapter (.row adapter) 0 ) 30)
-)
+  (> (.getValueAt adapter (.row adapter) 0 ) 30))
 
-(def tm (t/table-model :columns ["a" "v" ] :rows [[1 2] [ 2 3]]))
+(def menus (menubar :items [(menu :text "File" :items [ (menu-item :text "hello!")])]))
+
+
+(def tm (table-model :columns ["a" "v" ] :rows [[1 2] [ 2 3]]))
 (def tabl (table-x :model tm :show-grid? true :highlighters [((hl-color :foreground :red) hl-predicate)]))
 
-
-(s/config! fr :content (s/scrollable tabl))
-
-(-> fr s/pack! s/show!)
+(config! fr :content (scrollable tabl))
 
 
-(time (dotimes [i 10000]
-        (t/insert-at! tm i  [i (* 2 i)]))) 
+(config! fr :menubar menus)
 
-(t/value-at tm 1)
+(config! tabl :highlighters [
+                             ((hl-color :foreground :green) hl-predicate) 
+((hl-color :background "#ffc") 
+                                                                           (fn [renderer adapter] (odd? (.getValueAt adapter (.row adapter) 0)) ))])
+
+(config! tabl :highlighters nil)
+
+(-> fr pack! show!)
+
+(time (dotimes [i 100]
+        (insert-at! tm i  [i (* 2 i)]))) 
+
+;;(insert-at! tm 0 ["<html><b>FET</b></html>" 2])
+
+(insert-at! tm 0 [2222 33333])
+
+(insert-at! tm 0 ["<html><b>hej</b></html>" "<html>hahaha <i>hihihih</i></html>"])
+
+;; ≈Label label = new JLabel("<html><font color="FF0000">My</font> <font
+;; color="00FF00">label</font></html>");
+
+
+
+(value-at tm 1)
 
 ;; det går att lägga metadata varsomhelst
-
 ;;går det att påverka layouten i tabellen?
 
 (use 'seesaw.cells)
@@ -42,8 +62,7 @@
 
 (use 'clojure.reflect)
 
-
 (.getCellRenderer tabl 0 0)
-(s/config tabl)
+(config tabl)
 
-(s/config! fr :title "tablex")
+(config! fr :title "tablex")
