7f5de0374aed365fa18337a90f0d7d5d82c2ff30
Linus Ericsson <oscarlinusericsson@gmail.com>
2014-06-03 15:30:45 +0200

    added support for storing double arrays in a simple psql schema
    added some vector manipulation functions in mem.

--- src/clj/educational/persistence.clj
+++ src/clj/educational/persistence.clj
@@ -1,11 +1,26 @@
-(ns nebulosity.educational.persistence
-"persistence layer for PostgreSQL")
+(ns educational.persistence
+"persistence layer for PostgreSQL"
+(:require [clojure.java.jdbc :as sql]))
 
-;;TODO: drivers
+(def sql-settings "postgresql://linus:valkmon@localhost:5432/nebulosity")
 
-;;TODO: scheama
+(defn create-db []
+  (sql/db-do-commands sql-settings "create table vectorspace ( vector_id serial primary key, vector double precision []);"))
 
-;;TODO: store a 1000 long double vector
+(defn new-vector [double-vector]
+  {:pre [ (every? #(= (class %) java.lang.Double) double-vector) (= (count double-vector) 1000)]}
+  (sql/db-do-commands sql-settings 
+                      (doall
+                       (apply 
+                        str 
+                        "insert into vectorspace (vector) values ('{" 
+                        (apply str (interpose ", " 
+                                              (map #(Double/toString %) double-vector))) 
+                        "}');"))))
+
+(new-vector (vec (repeat 1000 0.99)))
+
+(sql/db-do-commands sql-settings "insert into vectorspace (vector) values ('{1.0, 2.0}')")
 
 ;;TODO: store taskblob
 
--- src/clj/educational/task.clj
+++ src/clj/educational/task.clj
@@ -1,3 +1,83 @@
-(ns nebulosity.educational.task
+(ns educational.task
   "a task description"
-)
+  (:use [clojure.data.generators :only [reservoir-sample]]))
+
+(def key-size 12)
+
+(def reservoir (apply vector-of :long (range 1000)))
+
+(defn generate-key 
+  "generate a random key"
+  []
+  (reservoir-sample key-size reservoir))
+
+(defn empty-vector 
+  "generate a 1000 double vector, all zeroes"
+  []
+  (apply vector-of :double (repeat 1000 (double 0))))
+
+;;TODO: transient vector operations.
+;;TODO: atomic vectors
+
+(defn generate-task 
+  "creates a task, 
+with its huge probability matrixes, 
+key and question/answer"
+  [question answer]
+  {:questions question
+   :answer answer
+   :key (generate-key)
+   :student-accomplish (empty-vector)
+   :student-fail (empty-vector)
+   :unsuccessful-next-tasks (empty-vector)
+   :successful-next-tasks (empty-vector)
+   :unsuccessful-previous-tasks (empty-vector)
+   :successful-previous-tasks (empty-vector)})
+
+(defn generate-student
+  "creates a new student with name, the random key
+vector for failed tasks, vector for successful tasks
+goal-vector for the student"
+  [name goal-vector]
+  {:name name
+   :key (generate-key)
+   :failed-tasks (generate-vector)
+   :successful-tasks (generate-vector)
+   :goal-vector (generate-vector)})
+
+(defn add-one [vector pos amount]
+  (assoc vector pos (+ (get vector pos) amount)))
+
+(defn add-key 
+  [vector key amount]  
+  {:pre [(vector? vector) (vector? key) (= (count key) key-size) (number? amount)]}
+  (reduce #(add-one %1 %2 amount) vector key))
+
+;;(add-key (generate-vector) (generate-key) 10)
+
+(defn ^String read-task [task]
+  (:question task))
+
+(defn correct-answer? [task answer]
+  (= (:answer task) answer))
+
+(defn write-back 
+  "when we know the result of the current task
+we can write it back to the previous task"
+  [previous-task current-task student correct?]
+  (add-key 
+   (get previous-task 
+        (if correct? 
+          :successful-next-task 
+          :unsuccessful-next-task))
+   (:key current-task)))
+
+(defn write-forward
+  "writes the result of the previous task to the current task"
+  [previous-task current-task student previous-correct?]
+  (add-key 
+   (get current-task 
+        (if correct? 
+          :successful-previous-task 
+          :unsuccessful-previous-task)) 
+   (:key previous-task)))
